f083b61305a250930c24aa84f8184c461f622669
michael240893 <michael.schimpelsberger@aon.at>
2021-05-14 21:27:58 +0200

    github repos and message backend

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar, Typography, Tooltip, Paper, Popper, ClickAwayListener, List, ListItem, ListItemText } from '@material-ui/core';
 import MainContainer from './MainContainer';
 import { useBackend } from './providers/BackendProvider';
 import PredictionComponent from './PredictionComponent';
@@ -9,6 +9,7 @@ import {useQuery} from 'react-query';
 import CenteredCircularProgress from './CenteredCircularProgress';
 import Message from "./Message";
 import AppInfoComponent from './AppInfoComponent';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -21,13 +22,37 @@ const useStyles = makeStyles((theme) => ({
   },
   header:{
       marginBottom:theme.spacing(2)
+  },
+  right:{
+      flex:1
+  },
+  popper:{
+    zIndex:1500,
   }
 
 }));
 
 const LOCATION_PATH="/locations";
-//const WIND_DIR_PATH="/wind_directions";
 
+
+function Menu (props) {
+    return (
+        <ClickAwayListener onClickAway={props.onClickAway}>
+            <Popper className={props.className}
+                placement={props.placement}
+                open={true}
+                anchorEl={props.anchorEl}
+            >
+                <Paper className={props.className} square elevation={4} >
+                    {props.children}
+                </Paper>
+            </Popper>
+        </ClickAwayListener>
+    )
+}
+
+const FLASK_BACKEND_REPO_URL="https://github.com/michael240893/flask-heroku";
+const REACT_FRONEND_REPO_URL="https://github.com/michael240893/react-dm-heroku";
 
 export default function MainComponent(props){
 
@@ -36,15 +61,19 @@ export default function MainComponent(props){
 
     const {apiCall}=useBackend();
 
+    const menuRef=React.useRef();
+    const [menuOpen, setMenuOpen]=React.useState(false);
+
     const locations=useQuery(
         LOCATION_PATH,
         ()=>apiCall(LOCATION_PATH,"get")
     )
+
+    const openRepository=(url)=>{
+        setMenuOpen(false)
+        window.open(url);
+    }
     
-    // const windDirections=useQuery(
-    //     WIND_DIR_PATH,
-    //     ()=>apiCall(WIND_DIR_PATH,"get")
-    // )
 
     return(
         <div>
@@ -53,6 +82,10 @@ export default function MainComponent(props){
                     <Typography variant="h5">
                         Weather Australia
                     </Typography>
+                    <div className={classes.right}/>
+                    <Tooltip title="Source Code">
+                        <IconButton onClick={()=>setMenuOpen(true)} ref={menuRef} color="inherit"><GitHubIcon/></IconButton>
+                    </Tooltip>
                 </Toolbar>
             </AppBar>    
             <AppInfoComponent/>   
@@ -62,10 +95,26 @@ export default function MainComponent(props){
                 {locations.error?<Message severity="error">An error occurred when trying to load the available locations from the server</Message>:null}
                 {locations.isLoading&&<CenteredCircularProgress/>}
                 {!locations.isLoading&&locations.data&&
+
                     <PredictionComponent
                         locations={locations.data}
                     />
                 }
+
+                {menuOpen&&<Menu
+                className={classes.popper}
+                placement={"bottom-end"}
+                anchorEl={menuRef.current}
+                onClickAway={()=>setMenuOpen(false)}>
+                    <List>
+                        <ListItem button onClick={()=>openRepository(FLASK_BACKEND_REPO_URL)}>
+                            <ListItemText disableTypography primary="Github Repository - Flask Backend"> </ListItemText>
+                        </ListItem>
+                        <ListItem button onClick={()=>openRepository(REACT_FRONEND_REPO_URL)}>
+                            <ListItemText disableTypography primary="Repository React - React Frontend"> </ListItemText>
+                        </ListItem>
+                    </List>
+            </Menu>}
 
             </MainContainer>  
         </div>
--- src/PredictionResult.jsx
+++ src/PredictionResult.jsx
@@ -1,10 +1,13 @@
 import {Box, Typography} from '@material-ui/core'; 
+import Message from "./Message";
 
 
 export default function PredictionResult(props){
 
     return (
         <Box align="center">
+            {props.result.subset&&<Message severity="warning">Due to memory restrictions for hobby accounts on Heroku, the model on the backend is currently only trained with a sample size {props.result.subset}. Please run the application on localhost and set the parameter 'MAX_ROWS' to 'None' in the file named 'main.py' if you want to use a model trained with the whole dataset!</Message>}
+
             {!props.result&&<Typography variant="h6" color="textSecondary">
                 No prediction loaded yet
             </Typography>}
@@ -12,6 +15,7 @@ export default function PredictionResult(props){
             <Typography variant="h2">
                 {props.result.rainNextDay}
             </Typography>
+
         </Box>
 
 
